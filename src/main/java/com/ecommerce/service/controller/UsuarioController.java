db19ca186db7e999ced07949d4a70cf06b73b787
DanielChabueta <chambuetadaniel@gmail.com>
2022-11-20 23:08:59 -0500

    Consumo de API para validar usuario

---
+++ src/main/java/com/ecommerce/service/controller/UsuarioController.java
@@ -0,0 +1,33 @@
+package com.ecommerce.service.controller;
+
+import com.ecommerce.service.entity.Usuario;
+import com.ecommerce.service.service.UsuarioService;
+import com.ecommerce.service.utils.GenericResponse;
+
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("api/usuario")
+public class UsuarioController {
+    private final UsuarioService service;
+
+    public UsuarioController(UsuarioService service) {
+        this.service = service;
+    }
+    @PostMapping("/login")
+    public GenericResponse<Usuario> login(HttpServletRequest request){
+        String email = request.getParameter("email");
+        String contrasenia = request.getParameter("pass");
+        return this.service.login(email, contrasenia);
+    }
+    @PostMapping
+    public GenericResponse save(@RequestBody Usuario u){
+        return this.service.guardarUsuario(u);
+    }
+    @PutMapping("/{id}")
+    public GenericResponse update(@PathVariable int id, @RequestBody Usuario u){
+        return this.service.guardarUsuario(u);
+    }
+}
